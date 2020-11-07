7a8e5841082e958db6636dfddd8957ba2503f998
FrankLeFebvre2023 <fjl4@njit.edu>
2020-11-06 20:58:14 -0500

    Tests survey creation

---
+++ project/test_create_survey.php
@@ -0,0 +1,52 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+
+<form method="POST">
+	<label>Title</label>
+	<input name="title" placeholder="title"/>
+	<label>Description</label>
+	<input name="description" placeholder = "description"/>
+	<label>Visibility</label>
+	<select name = "visibility">
+		<option value="0">Draft</option>
+		<option value="1">Private</option>
+		<option value="2">Public</option>
+	</select>
+	<input type="submit" name="save" value="Create"/>
+</form>
+
+<?php
+if(isset($_POST["save"])){
+	//TODO add proper validation/checks
+	$title = $_POST["title"];
+	$description = $_POST["description"];
+	$visibility = $_POST["visibility"];
+	//$created = date('Y-m-d H:i:s');
+	//$modified = date('Y-m-d H:i:s');
+	$user = get_user_id();
+	$db = getDB();
+	$stmt = $db->prepare("INSERT INTO Survey (title, description, visibility, user_id) VALUES(:title, :description, :visibility, :user)");
+	$r = $stmt->execute([
+		":title"=>$title,
+		":description"=>$description,
+		":visibility"=>$visibility,
+		//":created"=>$created,
+		//":modified"=>$modified,
+		":user"=>$user
+	]);
+	if($r){
+		flash("Created successfully with id: " . $db->lastInsertId());
+	}
+	else{
+		$e = $stmt->errorInfo();
+		flash("Error creating: " . var_export($e, true));
+	}
+}
+?>
+<?php require(__DIR__ . "/partials/flash.php");
